d5e861ecc3f0c5aea190965eb9a4c6e65597c38d
Ronnie Overby <roverby@core-techs.net>
2013-11-22 11:20:08 -0500

    null target nuget release

---
+++ CoreTechs.Logging/Targets/NullTarget.cs
@@ -0,0 +1,12 @@
+﻿using CoreTechs.Logging.Configuration;
+
+namespace CoreTechs.Logging.Targets
+{
+    [AliasTypeName("Null")]
+    public class NullTarget : Target
+    {
+        public override void Write(LogEntry entry)
+        {
+        }
+    }
+}
