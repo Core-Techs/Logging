88f58154738d2ff12cad39cc650ff6371c6bea21
Ronnie Overby <ronnie@ronnieo.net>
2015-08-15 18:48:11 -0400

    improvements to TraceTarget

--- CoreTechs.Logging/Targets/TraceTarget.cs
+++ CoreTechs.Logging/Targets/TraceTarget.cs
@@ -1,11 +1,12 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Xml.Linq;
 using CoreTechs.Logging.Configuration;
 
 namespace CoreTechs.Logging.Targets
 {
     [AliasTypeName("Trace")]
-    public class TraceTarget : Target, IConfigurable
+    public class TraceTarget : Target, IConfigurable, IFlushable
     {
         public IEntryConverter<string> EntryFormatter { get; set; }
 
@@ -13,13 +14,40 @@ namespace CoreTechs.Logging.Targets
         {
             var fmt = EntryFormatter ?? entry.Logger.LogManager.GetFormatter<string>();
             var msg = fmt.Convert(entry);
-            Trace.WriteLine(msg);
+
+            switch (entry.Level)
+            {
+                case Level.Trace:
+                case Level.Debug:
+                    Trace.WriteLine(msg);
+                    break;
+                case Level.Info:
+                    Trace.TraceInformation(msg);
+                    break;
+                case Level.Warn:
+                    Trace.TraceWarning(msg);
+                    break;
+                case Level.Error:
+                    Trace.TraceError(msg);
+                    break;
+                case Level.Fatal:
+                    Trace.Fail(msg);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
         }
 
         public void Configure(XElement xml)
         {
             EntryFormatter =
                 ConstructOrDefault<IEntryConverter<string>>(xml.GetAttributeValue("EntryFormatter", "Formatter"));
+        }
+
+        public void Flush()
+        {
+            Trace.Flush();
         }
     }
 }
